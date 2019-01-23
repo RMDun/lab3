0df8039aafcd113ee259a54a13e5da0d8e195287
Rachel Duncan <rmduncan@ucsd.edu>
2019-01-22 23:38:14 -0800

    Replacing instead of append

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -17,12 +17,25 @@ function initializePage() {
 	// example: $("#div-id").click(functionToCall);
 	$("a.thumbnail").click(projectClick);
 }
+
 function projectClick(e) {
 	e.preventDefault();
 	$(this).css("background-color", "#7fff00");
 
 	// appending
+	//  var containingProject = $(this).closest(".project");
+	//  .append("<div class='project-description'><p>Description of the 
+	//           project.</p></div>");
+
 	var containingProject = $(this).closest(".project");
-	containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+	var description = $(containingProject).find(".project-description");
+
+	if(description.length == 0) {
+		$(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+
+	}
+	else {
+		description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+	}
 
 }
